530d9af020758757a58da3c13dff79acc8e9539a
G-David-S <gds.archived@gmail.com>
2026-08-27 00:25:53 +0200

    learning how to code rn

--- Untitled.py
+++ Untitled.py
@@ -1,2 +1,19 @@
-print ("this is the start accualy")
-# his should not be here
+ticker = "READOR"
+shares = 10
+price = 333.30
+value = shares * price
+
+print ("Your own", shares, "shares of", ticker)
+print ("Positon value", value)
+
+tickers = ["READOR", "MSFT", "GOOGL"]
+shares = [10, 5, 8]
+prices = {
+    "READOR": 333.30,
+    "MSFT": 410.21,
+    "GOOGL": 167.81
+}
+
+print (tickers)
+print (tickers[0])
+print (prices["READOR"])
